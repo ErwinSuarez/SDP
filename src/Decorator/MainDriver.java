aefcda6543bbea510f9da0f6176ff3f04aa1dd85
Erwin <erwinsuarez2003@yahoo.com>
2020-01-05 17:35:21 +0000

    build sample exercise with for driver class

--- src/Decorator/MainDriver.java
+++ src/Decorator/MainDriver.java
@@ -1,4 +1,28 @@
 package Decorator;
 
 public class MainDriver {
+
+    public static void main(String[] args) {
+
+        PC basic = new BasicPC();
+        basic.buildPC();
+        System.out.println("\n");
+
+        PC home = new HomePC(basic);
+        home.buildPC();
+        System.out.println("\n");
+
+        /* This also works the same with the one above
+        PC home1 = new HomePC(new BasicPC());
+        home1.buildPC();
+        System.out.println("\n");
+        */
+
+        PC office = new OfficePC(home);
+        office.buildPC();
+        System.out.println("\n");
+
+        PC game = new GamingPC(office);
+        game.buildPC();
+    }
 }
