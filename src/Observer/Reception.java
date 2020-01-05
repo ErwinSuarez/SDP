fab977c6e52ff3869c3fcbcba487a76c0f6be795
Erwin <erwinsuarez2003@yahoo.com>
2020-01-05 19:02:13 +0000

    update Reception class with method overrides and postMessage method

--- src/Observer/Reception.java
+++ src/Observer/Reception.java
@@ -1,4 +1,42 @@
 package Observer;
 
-public class Reception {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reception implements Observable {
+
+    private List<Observing> observing;
+    private String messageRequest;
+    private String patient;
+
+    Reception(){
+        new ArrayList<>();
+    }
+
+    @Override
+    public void registerObserver(Observing observing) {
+            this.observing.add(observing);
+    }
+
+    @Override
+    public void removeObserver(Observing observing) {
+        this.observing.remove(observing);
+    }
+
+    @Override
+    public void notifyObserver() {
+        List<Observing> observers = new ArrayList<>(this.observing);
+        for(Observing observ: observers)
+            observ.update();
+    }
+
+    @Override
+    public Object getUpdate(Observing observing) {
+        return messageRequest;
+    }
+
+    public void postMessage(String msg){
+        System.out.println("New message: " + msg);
+        notifyObserver();
+    }
 }
