c68473eebbbfe1d3486778e32b5cd26b1ebb41d3
Erwin <erwinsuarez2003@yahoo.com>
2020-01-05 11:44:21 +0000

    implement PaymentStrategy interface and overide pay method

--- src/Strategy/CardPaymentStrategy.java
+++ src/Strategy/CardPaymentStrategy.java
@@ -1,4 +1,8 @@
 package Strategy;
 
-public class CardPaymentStrategy {
+public class CardPaymentStrategy implements PaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.printf("Payed €%.2f card", amount);
+    }
 }
