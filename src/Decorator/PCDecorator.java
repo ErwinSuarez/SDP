b90a9877306da2802b070ff1d027e8093e315972
Erwin <erwinsuarez2003@yahoo.com>
2020-01-05 16:06:47 +0000

    implemented PC interface and build the constructor

--- src/Decorator/PCDecorator.java
+++ src/Decorator/PCDecorator.java
@@ -1,4 +1,15 @@
 package Decorator;
 
-public class PCDecorator {
+public class PCDecorator implements PC {
+
+    private PC pc;
+
+    public PCDecorator(PC pc){
+        this.pc = pc;
+    }
+
+    @Override
+    public void buildPC() {
+        this.pc.buildPC();
+    }
 }
