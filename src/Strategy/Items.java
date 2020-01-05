241e215289ffc1376cc81c60630173d1410a16d8
Erwin <erwinsuarez2003@yahoo.com>
2020-01-05 13:52:27 +0000

    added variables, getter, setter and constructor

--- src/Strategy/Items.java
+++ src/Strategy/Items.java
@@ -1,4 +1,28 @@
 package Strategy;
 
 public class Items {
+
+    private String description;
+    private double cost;
+
+    public Items(String description, double cost) {
+        this.description = description;
+        this.cost = cost;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 }
