167a31170653fe6836ebdaf49bacaee70cdb7da1
Erwin <erwinsuarez2003@yahoo.com>
2020-01-05 15:59:33 +0000

    overridden buildPC method

--- src/Decorator/BasicPC.java
+++ src/Decorator/BasicPC.java
@@ -1,4 +1,10 @@
 package Decorator;
 
-public class BasicPC {
+public class BasicPC implements PC{
+
+    @Override
+    public void buildPC() {
+        System.out.println("Bare-essentials of a PC");
+        System.out.println("It has a CPU, RAM, Storage Media and a Motherboard");
+    }
 }
