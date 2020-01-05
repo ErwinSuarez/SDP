ce22da937ced29055eab663f0b1f24bbdbb28ac5
Erwin <erwinsuarez2003@yahoo.com>
2020-01-05 17:20:46 +0000

    created concrete implementations for OfficePC with method override

--- src/Decorator/OfficePC.java
+++ src/Decorator/OfficePC.java
@@ -1,4 +1,14 @@
 package Decorator;
 
-public class OfficePC {
+public class OfficePC extends PCDecorator {
+
+    public OfficePC(PC pc) {
+        super(pc);
+    }
+
+    @Override
+    public void buildPC() {
+        super.buildPC();
+        System.out.println("Office PC's come in different sizes for office use");
+    }
 }
