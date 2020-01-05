223f21033af20d641d14ae0ccc6556d9b3434871
Erwin <erwinsuarez2003@yahoo.com>
2020-01-05 11:52:07 +0000

    created sample to test main

--- src/Strategy/MainDriver.java
+++ src/Strategy/MainDriver.java
@@ -1,4 +1,21 @@
 package Strategy;
 
+import java.util.Scanner;
+
 public class MainDriver {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String paymentOption;
+
+        System.out.println("Welcome\nPlease select option below");
+        System.out.println("A - for cash payment");
+        System.out.println("B - for card payment");
+        System.out.print("Please choose payment option: ");
+        paymentOption = input.nextLine();
+
+        System.out.printf("You have selected %s for payment", paymentOption);
+
+    }
 }
