c6b663a7e7a9c7b6e83989a0904c0336747e85e7
Erwin <erwinsuarez2003@yahoo.com>
2020-01-05 15:48:10 +0000

    created PaymentType class to handle the different options

--- src/Strategy/CardPaymentStrategy.java
+++ src/Strategy/CardPaymentStrategy.java
@@ -2,14 +2,8 @@ package Strategy;
 
 public class CardPaymentStrategy implements PaymentStrategy {
 
-    private String nameOnCard = "John Doe";
-    private String carNumber = "4566987112368522";
-    private int cvv = 214;
-    private String expirationDate = "21/04/2022";
-
     @Override
     public void pay(double amount) {
         System.out.printf("Payed €%.2f card", amount);
-        System.out.printf("Using card  %s", nameOnCard);
     }
 }
--- src/Strategy/PaymentType.java
+++ src/Strategy/PaymentType.java
@@ -1,4 +1,14 @@
 package Strategy;
 
 public class PaymentType {
+
+    public static PaymentStrategy getPayment(String sType){
+        switch(sType){
+            case "a":
+                return new CashPaymentStrategy();
+            case "b":
+                return new CardPaymentStrategy();
+        }
+        throw new IllegalArgumentException();
+    }
 }
