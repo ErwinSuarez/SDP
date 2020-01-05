eab8e3142f470c712b242ae07300a714a5d12deb
Erwin <erwinsuarez2003@yahoo.com>
2020-01-05 17:22:06 +0000

    created concrete implementations for GamingPC with method override

--- src/Decorator/GamingPC.java
+++ src/Decorator/GamingPC.java
@@ -1,4 +1,14 @@
 package Decorator;
 
-public class GamingPC {
+public class GamingPC extends PCDecorator {
+
+    public GamingPC(PC pc) {
+        super(pc);
+    }
+
+    @Override
+    public void buildPC() {
+        super.buildPC();
+        System.out.println("Gaming PC's have high end components that deals with extra computational power");
+    }
 }
